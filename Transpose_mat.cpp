b3cc0b07aea1299725d2fbd70daeb60b92486a3f
MMPgenave <mohammadmp14@gmail.com>
2021-01-29 13:43:47 +0330

    new files

---
+++ Transpose_mat.cpp
@@ -0,0 +1,1 @@
+#include <iostream>using namespace std;void  trans33(double a[3][3]){ double b[3][3];for (int i=0;i<3;i++){for (int j=0;j<3;j++){b[j][i]=a[i][j];}}for (int i=0;i<3;i++){for (int j=0;j<3;j++){cout<<b[i][j]<<"  ";}cout<<"\n";}}int main(){double  a[3][3]={{1,2,3},{3,4,5},{6,7,8} };trans33(a);return 0;}
